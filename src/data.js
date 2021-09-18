48d7fc04a3c159c64ec508c20f4098401ce21a5f
Jason Bailey <jmbailey816@gmail.com>
2021-09-18 10:01:41 -0400

    images and data.js resorted

--- src/data.js
+++ src/data.js
@@ -1,10 +1,26 @@
 export const projects = [
   {
+    title: "Workout Tracker",
+    subtitle: "Javascript, Express, Mongoose",
+    description:
+      "View your workout statistics on the main page, the total weight lifted, time spent, calories burned, etc. On the dashboard, view the past 7 days of workouts in more detail. This app also works offline and will sync your data once reconnected.",
+    image: "./workout-tracker.jpg",
+    link: "https://github.com/Jason-M-Bailey/workouttracker",
+  },
+  {
+    title: "Budget Tracker",
+    subtitle: "Express, Mongoose",
+    description:
+      "Track your withdrawals and deposits with or without a internet connection so that your account balance is accurate when traveling.",
+    image: "./workout-tracker.jpg",
+    link: "https://github.com/Jason-M-Bailey/budgettrackers",
+  },
+  {
     title: "Employee Directory",
     subtitle: "React",
     description:
       "View a company's employee directory with contact data, while being able to sort and filter.",
-    image: "./project-1.gif",
+    image: "./employee-directory.jpg",
     link: "https://github.com/Jason-M-Bailey/employee-directory",
   },
   {
@@ -14,14 +30,6 @@ export const projects = [
       "A back end for an e-commerce site configured with a Express.js API to use Sequelize to interact with a MySQL database.",
     image: "./project-2.gif",
     link: "https://github.com/Jason-M-Bailey/ecommerce-backend"
-  },
-  {
-    title: "Budget Tracker",
-    subtitle: "Express, Mongoose",
-    description:
-      "Track your withdrawals and deposits with or without a internet connection so that your account balance is accurate when traveling.",
-    image: "./project-3.gif",
-    link: "https://github.com/Jason-M-Bailey/budgettrackers",
   },
   {
     title: "Reading List",
@@ -38,14 +46,6 @@ export const projects = [
       "A retro grid-based game in vanilla JavaScript, HTML and CSS",
     image: "./project-4.gif",
     link: "https://github.com/Jason-M-Bailey/whack-a-mole",
-  },
-  {
-    title: "Workout Tracker",
-    subtitle: "Javascript, Express, Mongoose",
-    description:
-      "View your workout statistics on the main page, the total weight lifted, time spent, calories burned, etc. On the dashboard, view the past 7 days of workouts in more detail. This app also works offline and will sync your data once reconnected.",
-    image: "./project-4.gif",
-    link: "https://github.com/Jason-M-Bailey/workouttracker",
   },
 
 
